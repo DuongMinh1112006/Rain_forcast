dff07fee627d9133c126770705a06dbb153dfad1
Nguyễn Dương Minh <nguyenduongminh1112006@gmail.com>
2026-04-30 16:39:31 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import os 
+from twilio.rest import Client
+
+API_KEY =os.environ.get("Api_key")
+CNT = 4
+parameters= {
+    "lat" : 21.448250,
+    "lon" : 106.198860,
+    "appid" : API_KEY,
+    "cnt" : CNT
+}
+
+account_sid = os.environ.get("acc_sid")
+auth_token = os.environ.get("auth_token")
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+weather_id = [weather_data['list'][i]['weather'][0]['id'] for i in range(CNT)]
+for w_id in weather_id:
+    if w_id <700:
+        will_rain = True
+
+if  will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an umbrella ☂",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+84818265472"
+    )
+    print(message.status)
+
