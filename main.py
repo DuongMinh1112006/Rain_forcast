01c84afd440e6b65f695681922837b134dec1edf
Nguyễn Dương Minh <nguyenduongminh1112006@gmail.com>
2026-05-11 18:37:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 API_KEY =os.environ.get("API_KEY")
 CNT = 4
 parameters= {
-    "lat" : 20.980218,
-    "lon" : 105.787884,
+    "lat" : 8.247190,
+    "lon" : 34.591599,
     "appid" : API_KEY,
     "cnt" : CNT
 }
